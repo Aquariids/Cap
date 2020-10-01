5ea9092b3834c873672f6d755d76ebc97c6b9f00
Aquariids <70824286+Aquariids@users.noreply.github.com>
2020-10-01 05:37:17 +0400

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+$(function(){
+    $('.fairy-tail__sliders, .our-trip__sliders' ).slick({
+        prevArrow:'<button type="button" class="slick-btn slick-prev"><img src="/images/arrow-prev.svg" alt="arrow-prev"></button>',
+        nextArrow:'<button type="button" class="slick-btn slick-next"><img src="/images/arrow-next.svg" alt="arrow-next"></button>',
+        autoplay: true,
+        fade:true
+        
+    });
+    
+    
+});
+
